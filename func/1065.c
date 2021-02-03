b16e174464d20c1cd907fa2a52541bc391dc764a
이관희 <akrnrnak@gmail.com>
2021-02-03 16:45:47 +0900

    1065

---
+++ func/1065.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    int count = 0;
+    int arr[4];
+    int index;
+    int prev;
+
+    scanf("%d", &N);
+
+    for(int i = 1; i <= N; i++)
+    {
+        prev = index = 0;
+        if(i < 100)
+            count++;
+        else
+        {
+            for(int j = i; j != 0; j /= 10)
+            arr[index++] = j % 10;
+
+            for(int k = 0; k < index - 1; k++)
+                if((k == 0) || ((arr[k] - arr[k + 1]) == prev))
+                    if(k == index - 2)
+                        count++;
+                    else
+                    {
+                        prev = arr[k] - arr[k + 1];
+                        continue;
+                    }
+                else
+                    break;
+        }
+    }
+    printf("%d", count);
+
+    return 0;
+}
