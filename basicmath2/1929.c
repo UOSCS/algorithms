4ec5527c06d812be580c4a60ce3af0c42710b840
이관희 <akrnrnak@gmail.com>
2021-02-18 14:09:23 +0900

    1929

---
+++ basicmath2/1929.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+	int M, N;
+	bool* PrimeArray;
+
+	scanf("%d %d", &M, &N);
+
+	PrimeArray = malloc(sizeof(bool) * (N + 1));
+
+	memset(PrimeArray, true, N + 1);
+	memset(PrimeArray, false, 2);
+
+	for (int i = 2; i * i <= N; i++) 
+    {
+		if (PrimeArray[i]) 
+        {
+			for (int j = i * i; j <= N; j += i)
+				PrimeArray[j] = false;
+		}
+	}
+
+	for (int i = M; i <= N; i++) 
+    {
+		if (PrimeArray[i])
+			printf("%d\n", i);
+	}
+
+	free(PrimeArray);
+
+	return 0;
+}
