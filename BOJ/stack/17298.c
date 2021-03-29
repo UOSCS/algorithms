5b8c2a037f6dbd838be768178220c00102c8d2a6
이관희 <akrnrnak@gmail.com>
2021-03-29 10:35:46 +0900

    1874, 17298

---
+++ BOJ/stack/17298.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int *data;
+    int top;
+} Stack;
+
+int main(void)
+{
+    Stack s;
+    int N, *arr, *result;
+
+    scanf("%d", &N);
+    arr = malloc(sizeof(int) * N);
+    result = malloc(sizeof(int) * N);
+    s.data = malloc(sizeof(int) * N);
+    for(int i = 0; i < N; i++)
+        scanf("%d", arr + i);
+
+    s.top = -1;
+    for(int i = N - 1; i >= 0; i--)
+    {
+        if(s.top == -1)
+        {
+            s.data[++(s.top)] = arr[i];
+            result[i] = -1;
+            continue;
+        }
+        while(arr[i] >= s.data[s.top] && s.top != -1)
+            s.top--;
+
+        if(s.top == -1)
+            result[i] = -1;
+        else
+            result[i] = s.data[s.top];
+        s.data[++(s.top)] = arr[i];
+    }
+    for(int i = 0; i < N; i++)
+        printf("%d ", result[i]);
+
+    return 0;
+}
---
+++ BOJ/stack/1874.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define SIZE 100000
+
+typedef struct
+{
+    int data[SIZE];
+    int top;
+} Stack;
+
+int main(void)
+{
+    Stack s;
+    int n, arr[SIZE];
+    char result[SIZE * 2];
+
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", arr + i);
+
+    s.top = -1;
+    for(int i = 0, result_index = 0, sequence = 1; i < n; i++)
+    {
+        while(s.top == -1 || arr[i] > s.data[s.top])
+        {
+            result[result_index++] = '+';
+            s.data[++(s.top)] = sequence++;
+        }
+        if(arr[i] == s.data[s.top])
+        {
+            result[result_index++] = '-';
+            (s.top)--;
+        }
+        else
+        {
+            printf("NO\n");
+            return 0;
+        }
+    }
+    for(int i = 0; i < 2 * n; i++)
+        printf("%c\n", result[i]);
+
+    return 0;
+}
