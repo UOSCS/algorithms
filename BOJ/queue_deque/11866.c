c3db19ebba42a689beb004ba59972b390b7a78e8
이관희 <akrnrnak@gmail.com>
2021-03-29 19:04:43 +0900

    2164, 11866

--- BOJ/queue_deque/11866.c
+++ BOJ/queue_deque/11866.c
@@ -14,11 +14,6 @@ void init(Queue *q)
     q->front = q->rear = 0;
 }
 
-int is_empty(Queue *q)
-{
-    return q->front == q->rear;
-}
-
 void enqueue(Queue *q, int element)
 {
     q->rear = (q->rear + 1) % (N + 1);
@@ -27,9 +22,6 @@ void enqueue(Queue *q, int element)
 
 int dequeue(Queue *q)
 {
-    if(is_empty(q))
-        return -1;
-
     q->front = (q->front + 1) % (N + 1);
     return q->element[q->front];
 }
@@ -46,29 +38,35 @@ int size(Queue *q)
 
 void front(Queue *q)
 {
-    printf("%d\n", is_empty(q) ? -1 : q->element[(q->front + 1) % (N + 1)]);
+    printf("%d, ", q->element[(q->front + 1) % (N + 1)]);
 }
 
 int main(void)
 {
     Queue *q = malloc(sizeof(Queue));
     int K;
-    
+
     init(q);
 
-    scanf("%d", &N, &K);
+    scanf("%d %d", &N, &K);
 
     q->element = malloc(sizeof(int) * (N + 1));
     for(int i = 1; i <= N; i++)
         enqueue(q, i);
+    printf("<");
     while(size(q) != 1)
-    {
-        int tmp;
-        dequeue(q);
-        tmp = dequeue(q);
-        enqueue(q, tmp);
-    }
-    front(q);
+        for(int j = 0, tmp; j < K; j++)
+            if(j < K - 1)
+            {
+                tmp = dequeue(q);
+                enqueue(q, tmp);
+            }
+            else
+            {
+                front(q);
+                dequeue(q);
+            }
+    printf("%d>\n", dequeue(q));
 
     return 0;
 }
