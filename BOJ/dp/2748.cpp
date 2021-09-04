ec4c7a8765f3652a510c17900878eb4e14443610
UOSCS <akrnrnak@gmail.com>
2021-09-04 18:27:21 +0900

    2748

---
+++ BOJ/dp/2748.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[91];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+
+    int n;
+
+    cin >> n;
+
+    dp[0] = 0, dp[1] = 1;
+
+    for(int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+    
+    cout << dp[n];
+
+    return 0;
+}
