acafe6aff9c78c6aeb1f1ef40690219890d49c04
UOSCS <akrnrnak@gmail.com>
2021-10-14 14:41:44 +0900

    1247

---
+++ BOJ/math/1247.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+
+    for(int i = 0; i < 3; i++)
+    {
+        int N;
+
+        cin >> N;
+
+        long long answer = 0LL;
+
+        int overflow = 0;
+
+        for(int j = 0; j < N; j++)
+        {
+            long long n;
+            
+            cin >> n;
+
+            if(answer > LONG_LONG_MAX - n) overflow++;
+            else if(answer < LONG_LONG_MIN - n) overflow--;
+
+            answer += n;
+        }
+
+        cout << ((overflow >= 0 ? (overflow == 0 ? (answer >= 0 ? (answer == 0 ? '0' : '+') : '-') : '+') : '-')) << '\n';
+    }
+
+    return 0;
+}
