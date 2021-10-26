08f4d103bd83531279a56136a276245e8fb874b3
UOSCS <akrnrnak@gmail.com>
2021-10-26 11:30:41 +0900

    1402

---
+++ BOJ/math/1402.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T;
+
+    cin >> T;
+
+    while(T--)
+    {
+        int A, B;
+
+        cin >> A >> B;
+
+        cout << "yes\n";
+    }
+
+    return 0;
+}
