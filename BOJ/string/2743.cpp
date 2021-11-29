2ec14b6dbb66c61276aa9ec223215ec33aa33ac6
UOSCS <akrnrnak@gmail.com>
2021-11-29 09:37:58 +0900

    '2743'

---
+++ BOJ/string/2743.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+
+    string str;
+
+    cin >> str;
+
+    cout << str.length();
+
+    return 0;
+}
